7350274528f4e24327a7dff871132d75abac8bdf
Ryan Wong <56795999+ryanwong2000@users.noreply.github.com>
2021-05-02 21:50:44 -0400

    issa comment

--- bot.js
+++ bot.js
@@ -15,17 +15,18 @@ function readyDiscord(){
 
 const osamakeReminder = require("./osamakeReminder.js");
 const testReminder = require("./testReminder.js");
+//import a function w all the cron jobs there?
 client.on('ready', () => {
     console.log('reminders');
-    remindersChannel = client.channels.cache.get(`831923281028317290`);
-    
+    // remindersChannel = client.channels.cache.get(`831923281028317290`);
+    cron.schedule('* * * * * *', testReminder);
     cron.schedule('0 12 * * wed', osamakeReminder);
     cron.schedule('0 11 * * wed', () => {
         remindersChannel.send("Super Cub widePeepoHappy");
         console.log("Super Cub widePeepoHappy");
     })
     cron.schedule('5 12 * * fri', testReminder);
-
+    
     // cron.schedule('24 10 * * thu', () => {
     //     remindersChannel.send("Minami 🐐🐐🐐🐐");
     //     console.log("Minami 🐐🐐🐐🐐");

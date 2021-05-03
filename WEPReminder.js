4f03d6ca8f92db8425f2b933d36b0564831fb64b
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2021-05-03 01:02:07 -0400

    delete

--- WEPReminder.js
+++
@@ -1,8 +0,0 @@
-//cron is a scheduler (node-cron)
-var CronJob = require('cron').CronJob;
-var job = new CronJob('00 8 * * 2', );
-job.start();
-
-module.exports = function WEPReminder(){
-    msg.channel.send("WONDER EGG TUESDAY!");
-}
--- osamakeReminder.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(){
-    const remindersChannel = client.channels.cache.get(`831923281028317290`);
-    if(remindersChannel){
-        console.log("osamake reminder");
-        remindersChannel.send("New Osamake episode today!");
-    }else{
-        console.log("ERROR: something wrong osamkeReminder");
-    }
-}

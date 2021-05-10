9c122102b4423d340ef95fbbe11916f36b2459c7
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2021-05-09 20:46:58 -0400

    fn clean up

--- reminders.js
+++ reminders.js
@@ -1,50 +1,34 @@
 //cron is a scheduler (node-cron)
-const cron = require('node-cron');
+const cron = require("node-cron");
 
 const shows = {
-    mtd: { crontab: '* * * * * *', message: "BOOBA" },
-    osamake: { crontab: '0 9 * * wed', message: "New Osamake episode today!" },
-    superCub: { crontab: '0 11 * * wed', message: "Super Cub widePeepoHappy" },
-    higehiro: { crontab: '0 11 * * mon', message: "New Higehiro today!" },
-    vivy: { crontab: '0 12 * * sat', message: "Vivy AYAYAJAM" }
+  mtd: { crontab: "* * * * * *", message: "BOOBA" },
+  osamake: { crontab: "0 9 * * wed", message: "New Osamake episode today!" },
+  superCub: { crontab: "0 11 * * wed", message: "Super Cub widePeepoHappy" },
+  higehiro: { crontab: "0 11 * * mon", message: "New Higehiro today!" },
+  vivy: { crontab: "0 12 * * sat", message: "Vivy AYAYAJAM" },
+};
+function reminder(remindersChannel, show) {
+  cron.schedule(show.crontab, () => {
+    console.log(show.message);
+    remindersChannel.send(show.message);
+  });
 }
-
 //its messy i know, could store pass a 'show' object that has all the relevant information a general function.
 module.exports = {
-    mtd: function(remindersChannel) {
-        let show = shows.mtd;//change show here
-        cron.schedule(show.crontab, ()=>{
-            console.log(show.message);
-            remindersChannel.send(show.message);
-        })
-    },
-    osamake: function(remindersChannel) {
-        let show = shows.osamake;//change show here
-        cron.schedule(show.crontab, ()=>{
-            console.log(show.message);
-            remindersChannel.send(show.message);
-        })
-    },
-    superCub: function(remindersChannel) {
-        let show = shows.superCub;//change show here
-        cron.schedule(show.crontab, ()=>{
-            console.log(show.message);
-            remindersChannel.send(show.message);
-        })
-    },
-    higehiro: function(remindersChannel) {
-        let show = shows.higehiro;//change show here
-        cron.schedule(show.crontab, ()=>{
-            console.log(show.message);
-            remindersChannel.send(show.message);
-        })
-    },
-    vivy: function(remindersChannel) {
-        let show = shows.vivy;//change show here
-        cron.schedule(show.crontab, ()=>{
-            console.log(show.message);
-            remindersChannel.send(show.message);
-        })
-    }
+  mtd: (remindersChannel) => {
+    reminder(remindersChannel, shows.mtd);
+  },
+  osamake: (remindersChannel) => {
+    reminder(remindersChannel, shows.osamake);
+  },
+  superCub: (remindersChannel) => {
+    reminder(remindersChannel, shows.superCub);
+  },
+  higehiro: (remindersChannel) => {
+    reminder(remindersChannel, shows.higehiro);
+  },
+  vivy: (remindersChannel) => {
+    reminder(remindersChannel, shows.higehiro);
+  },
 };
-

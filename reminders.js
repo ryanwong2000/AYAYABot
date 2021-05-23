2fddfb816c2dbdc8eebb4eae37cc411c29734ef3
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2021-05-22 22:47:43 -0400

    vivy

--- reminders.js
+++ reminders.js
@@ -1,12 +1,12 @@
 //cron is a scheduler (node-cron)
-const cron = require("node-cron");
+const cron = require('node-cron');
 
 const shows = {
-  mtd: { crontab: "* * * * * *", message: "BOOBA" },
-  osamake: { crontab: "0 9 * * wed", message: "New Osamake episode today!" },
-  superCub: { crontab: "0 11 * * wed", message: "Super Cub widePeepoHappy" },
-  higehiro: { crontab: "0 11 * * mon", message: "New Higehiro today!" },
-  vivy: { crontab: "0 12 * * sat", message: "Vivy AYAYAJAM" },
+  mtd: { crontab: '* * * * * *', message: 'BOOBA' },
+  osamake: { crontab: '0 9 * * wed', message: 'New Osamake episode today!' },
+  superCub: { crontab: '0 11 * * wed', message: 'Super Cub widePeepoHappy' },
+  higehiro: { crontab: '0 11 * * mon', message: 'New Higehiro today!' },
+  vivy: { crontab: '0 12 * * sat', message: 'Vivy AYAYAJAM' }
 };
 function reminder(remindersChannel, show) {
   cron.schedule(
@@ -15,7 +15,7 @@ function reminder(remindersChannel, show) {
       console.log(show.message);
       remindersChannel.send(show.message);
     },
-    { timezone: "America/New_York" }
+    { timezone: 'America/New_York' }
   );
 }
 //its messy i know, could store pass a 'show' object that has all the relevant information a general function.
@@ -34,6 +34,6 @@ module.exports = {
     reminder(remindersChannel, shows.higehiro);
   },
   vivy: (remindersChannel) => {
-    reminder(remindersChannel, shows.higehiro);
-  },
+    reminder(remindersChannel, shows.vivy);
+  }
 };

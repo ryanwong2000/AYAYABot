15b450eea05fd7b6ac786b1be2befd9585f08c4d
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2021-07-12 20:08:53 -0400

    disabled reminders

--- bot.js
+++ bot.js
@@ -1,29 +1,29 @@
-console.log("Ara araa");
+console.log('Ara araa');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOTTOKEN);
 
-client.on("ready", readyDiscord);
+client.on('ready', readyDiscord);
 
 function readyDiscord() {
-  console.log("ohayo U.U");
+  console.log('ohayo U.U');
 }
 
 //text channel commands
-const commandHandler = require("./commands");
-client.on("message", commandHandler);
+const commandHandler = require('./commands');
+client.on('message', commandHandler);
 
 //automatic reminders usign node cron
 //exported function calls from inside an anon fn
-const reminders = require("./reminders");
-client.on("ready", () => {
-  const channel = client.channels.cache.get(`831923281028317290`);
-  // reminders.mtd(channel);
-  reminders.osamake(channel);
-  reminders.superCub(channel);
-  reminders.higehiro(channel);
-  reminders.vivy(channel);
-});
+// const reminders = require("./reminders");
+// client.on("ready", () => {
+//   const channel = client.channels.cache.get(`831923281028317290`);
+//   // reminders.mtd(channel);
+//   reminders.osamake(channel);
+//   reminders.superCub(channel);
+//   reminders.higehiro(channel);
+//   reminders.vivy(channel);
+// });
